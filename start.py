406c5e75a23985212ac888f2ed06fc26d6fc7899
Ivan <mynamekasatkin@gmail.com>
2018-05-15 15:08:01 +0300

    Собираюсь обернуть в теги ul

--- start.py
+++ start.py
@@ -52,31 +52,46 @@ import json
 #22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
 
 
+# my_json = '''
+# 		[
+# 			{
+# 			 "h3": "Title #1",
+# 			 "div": "Hello, World 1!"
+# 			}
+
+# 		]
+# 		'''
+# my_json = json.loads(my_json)
+
+# html = ''
+# for set_tag in my_json:
+# 	for tag, value in set_tag.items():
+# 		html += "<{0}>{1}</{0}>".format(tag, value)
+# print(html)
+
+
+#3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+
 my_json = '''
 		[
 			{
-			 "h3": "Title #1",
-			 "div": "Hello, World 1!"
+			"h3": "Title #1",
+			"div": "Hello, World 1!"
+			},
+			{
+			"h3": "Title #2",
+			"div": "Hello, World 2!"
 			}
-
 		]
 		'''
+html = ""
 my_json = json.loads(my_json)
-
-html = ''
-for set_tag in my_json:
-	for tag, value in set_tag.items():
-		html += "<{0}>{1}</{0}>".format(tag, value)
+json_type = type(my_json)
+print(len(my_json))
+count = 0
+if json_type == list:
+	for set_tag in my_json:
+		for tag, value in set_tag.items():
+			html += "<li><{0}>{1}</{0}></li>".format(tag, value)
 print(html)
-
-
-
-
-
-
-
-
-
-
-
-
